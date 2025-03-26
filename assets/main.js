c9cc001c1a7d326a12fd99bfa207baee7758f86f
andrea-maiolo <maiolo.m.2@gmail.com>
2025-03-26 16:09:57 +0100

    added functionality and basic style

--- assets/main.js
+++ assets/main.js
@@ -1,14 +1,41 @@
 const inputTask = document.getElementById("inputTask");
-
-// inputTask.addEventListener("change",);
+const mainDiv = document.getElementById("main");
 
 const addTask = function (e) {
-  console.dir(e.target.parentNode);
+  e.preventDefault();
+  if (inputTask.value === "") {
+    alert("una task deve avere un nome");
+    return;
+  }
+
+  let myDiv = document.createElement("div");
+  let myPara = document.createElement("p");
+  let myButton = document.createElement("button");
+
+  myPara.innerText = inputTask.value;
+  myButton.innerText = "Rimuovi";
+  myButton.classList.add("trash");
+
+  myButton.addEventListener("click", removeTask);
+  myDiv.appendChild(myPara);
+  myDiv.appendChild(myButton);
+  myDiv.classList.add("singleTask");
+  myDiv.addEventListener("click", taskCompleted);
+
+  mainDiv.appendChild(myDiv);
 };
 
 const buttonAdd = document.querySelector("form button");
 buttonAdd.addEventListener("click", addTask);
 
-// buttonAdd.addEventListener("click", function (e) {
-//   e.preventDefault();
-// });
+const removeTask = function (e) {
+  let divParent = e.target.parentElement;
+  mainDiv.removeChild(divParent);
+};
+
+const taskCompleted = function (e) {
+  let currentDiv = e.currentTarget;
+  currentDiv.classList.toggle("divCompleted");
+  let paraCompleted = e.currentTarget.children[0];
+  paraCompleted.classList.toggle("paraCompleted");
+};
